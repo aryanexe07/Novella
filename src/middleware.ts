2c0506088e245ecf62e0ea434705a57bfb06c6d2
Aryan Tailor <aryantailor.me@gmail.com>
2026-07-04 15:44:07 +0530

    feat: add clerkMiddleware to secure routes with Next.js matcher configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.[\\w]+$|_next/image|favicon.ico).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next|*\\..*).*)"],
 };
